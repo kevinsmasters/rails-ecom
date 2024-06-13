411274558b9c4c3755bd60c5dc56c5af1558facb
Kevin Masters <irimiuke@gmail.com>
2024-06-12 20:56:37 -0400

    dashboard revenue card

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,14 +4,14 @@ class AdminController < ApplicationController
   def index
     @orders = Order.where(fulfilled: false).order(created_at: :desc).take(5)
     @quick_stats = {
-      sales: Order.where(created: Time.now.midnight..Time.now).count,
-      revenue: Order.where(created: Time.now.midnight..Time.now).sum(:total).round(),
-      avg_sale: Order.where(created: Time.now.midnight..Time.now).average(:total).round(),
-      per_sale: OrderProduct.joins(:order).where(orders: { created_at: Time.now.midnight..Time.now }).average(:quantity)
+      sales: Order.where(created_at: Time.now.midnight..Time.now).count,
+      revenue: Order.where(created_at: Time.now.midnight..Time.now).sum(:total)&.round(),
+      avg_sale: Order.where(created_at: Time.now.midnight..Time.now).average(:total)&.round(),
+      per_sale: OrderProduct.joins(:order).where(orders: { created_at: Time.now.midnight..Time.now })&.average(:quantity)
     }
     @orders_by_day = Order.where('created_at > ?', Time.now - 7.days).order(:created_at)
     @orders_by_day = @orders_by_day.group_by { |order| order.created_at.to_date }
-    @revenue_by_day = @orders_by_day.map { |day, orders| [day.strftime("%A"), order.sum(&:total)] }
+    @revenue_by_day = @orders_by_day.map { |day, orders| [day.strftime("%A"), orders.sum(&:total)] }
     if @revenue_by_day.count < 7
       days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
