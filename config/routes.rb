526363d7e374966655efcb4408bc71d78f73cf6b
Kevin Masters <irimiuke@gmail.com>
2024-06-18 20:37:21 -0400

    add categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
     root to: "admin#index", as: :admin_root
   end
 
+  resources :categories, only: [:show]
+
   get "admin" => "admin#index"
-  get "category" => "category#index"
 end
