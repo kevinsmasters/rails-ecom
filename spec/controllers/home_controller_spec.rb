d94fc5678f2702cd6cd33640ec7a53db09de2df1
Kevin Masters <irimiuke@gmail.com>
2024-07-01 20:16:50 -0400

    home spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe HomeController do
   describe '#index' do
-    # (1..6).each do |i|
-    #   Category.create(name: "Category #{i}", description: "Category description #{i}")
-    # end
+    (1..6).each do |i|
+      Category.create(name: "Category #{i}", description: "Category description #{i}")
+    end
     it 'takes 4 categories' do
       # take 4 categories
-      expect(false).to be(true)
+      expect(@main_categories.count).to be(4);
     end
   end
 end
